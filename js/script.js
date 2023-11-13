3607edbf64ccc380aba997ae65fbc7c4fcc8a7ae
Mikel Apraiz <apraiz.mikel@gmail.com>
2023-11-13 22:49:25 +0100

    entregable terminado

--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@ import peliculas from './peliculas.js'
 
 /*3. Haz que en cada sección aparezca la imagen y el título de la pelicula filtrado por el genero */
 
-const accion = peliculas.filter(p => p.genre_ids.find(g => g ==28) != undefined);
-const imagenCompleta = accion.map(imagen => `https://image.tmdb.org/t/p/w500` + imagen.poster_path);
+const accion = peliculas.filter(p => p.genre_ids.find(g => g ==28));
+
 const peliculasAccion = accion.map(pelicula => 
-    {return '<li>'+pelicula.title+'</li>' })
+    {return `<div><img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}"/><li>${pelicula.title}</li></div>`})
 
 document.getElementById("genero-28").innerHTML = peliculasAccion;
 
-
-
-const Thriller = peliculas.filter(p => p.genre_ids.find(g => g ==53) != undefined);
+const Thriller = peliculas.filter(p => p.genre_ids.find(g => g ==53));
 const peliculasThriller = Thriller.map(pelicula => 
-    {return '<li>'+pelicula.title+'</li>' })
+    {return `<div><img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}"/><li>${pelicula.title}</li></div>`})
 
 document.getElementById("genero-53").innerHTML = peliculasThriller;
 
-const Aventura = peliculas.filter(p => p.genre_ids.find(g => g ==12) != undefined);
+const Aventura = peliculas.filter(p => p.genre_ids.find(g => g ==12));
 const peliculasAventura = Aventura.map(pelicula => 
-    {return '<li>'+pelicula.title+'</li>' })
+    {return `<div><img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}"/><li>${pelicula.title}</li></div>`})
 
 document.getElementById("genero-12").innerHTML = peliculasAventura;
